2d19a9cb0e143764d68b975dd4c53e77246f4dd7
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-08-15 13:23:57 +0300

    Add alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+
+-- Authors Table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Books Table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    price DOUBLE,
+    published_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Customers Table
+CREATE TABLE Customers (
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+-- Orders Table
+CREATE TABLE Orders (
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+	order_date DATE,
+	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order_Details Table
+CREATE TABLE Order_Details (
+	orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+	quantity DOUBLE,
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+	FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
